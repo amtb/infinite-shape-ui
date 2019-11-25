7bbfb16ff19e0b8743dffc111efb20b5ff40f094
Alpha BARRY <alpha.mt.barry@gmail.com>
2019-11-25 16:09:32 +0400

    Icons icons icons

--- src/app/shape/shape.module.ts
+++ src/app/shape/shape.module.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material';
 import { ShapeComponent } from './shape.component';
-
-
 
 @NgModule({
   declarations: [ShapeComponent],
   exports: [ShapeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ]
 })
 export class ShapeModule { }
